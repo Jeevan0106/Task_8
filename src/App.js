6676b00a62610660f441d8b96c0885d9f1f5e338
Jeevanprasath.E <e.jeevanprasath@gmail.com>
2024-11-27 10:28:14 +0530

    change the name

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ const CRUD_UI = () => {
       {/* Navbar */}
       <nav className="navbar navbar-dark bg-dark">
         <div className="container-fluid">
-          <span className="navbar-brand mb-0 h1">Me Call API - CRUD</span>
+          <span className="navbar-brand mb-0 h1">API - CRUD</span>
           <Button variant="primary" onClick={() => handleOpenModal()}>
             Create
           </Button>
